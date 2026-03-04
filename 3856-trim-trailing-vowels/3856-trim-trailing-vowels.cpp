de44dfbf8cab505d8db390ab658e8222c4485bce
Ananya Srivastava <124052723+AnanyaSrivastava1203@users.noreply.github.com>
2026-03-04 06:17:03 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3856-trim-trailing-vowels/3856-trim-trailing-vowels.cpp
+++ 3856-trim-trailing-vowels/3856-trim-trailing-vowels.cpp
@@ -2,16 +2,9 @@ class Solution {
 public:
     string trimTrailingVowels(string s) {
         int n=s.length();
-        string res="";
-        for(int i=n-1;i>=0;i--){
-            if(s[i]=='a'||s[i]=='e'||s[i]=='i'||s[i]=='o'||s[i]=='u'){
-                continue;
-            }
-            else{
-                res=s.substr(0,i+1);
-                break;
-            }
-        }
-        return res;
+        int i=n-1;
+        while(i>=0 && (s[i]=='a'||s[i]=='e'||s[i]=='o'||s[i]=='i'||s[i]=='u'))
+        i--;
+        return s.substr(0,i+1);
     }
 };
