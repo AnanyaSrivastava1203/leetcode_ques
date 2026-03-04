f0cf2267c81376c0250b4c0e4898386b7b2dac24
Ananya Srivastava <124052723+AnanyaSrivastava1203@users.noreply.github.com>
2026-03-04 06:39:09 +0530

    Time: 0 ms (100%), Space: 7.9 MB (99.93%) - LeetHub

---
+++ 3857-minimum-cost-to-split-into-ones/3857-minimum-cost-to-split-into-ones.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int minCost(int n) {
+        //n=(n-1)+1=>cost=n-1
+        //n-1=(n-2)+1=>cost=n-2
+        //n-2=(n-3)+1=>n-3
+        //...
+        //3=2+1=>cost=2
+        //2=1+1=>cost=1
+        //total cost=1+2+3+..+n-1=(n-1)(n)/2
+        return n*(n-1)/2;
+    }
+};
