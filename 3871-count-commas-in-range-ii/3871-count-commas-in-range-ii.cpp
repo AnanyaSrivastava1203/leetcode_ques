9d890195c6ffebea1cb21ce2d151fef77df06d45
Ananya Srivastava <124052723+AnanyaSrivastava1203@users.noreply.github.com>
2026-03-15 23:35:09 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.cpp
+++ 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.cpp
@@ -2,17 +2,17 @@ class Solution {
 public:
     long long countCommas(long long n) {
         long long ans=0;
-        for(int i=4;i<=18;i++){
-            long long commas=(i-1)/3;
-
-            long long left=pow(10,i-1);
-            long long right=pow(10,i)-1;
+        for(int i=4;i<=16;i++){//iterting over digit lengths
+            long long commas=(i-1)/3;//number of commas when digit is i
+            //now calculate the range of numbers with exactly i digits
+            long long left=pow(10,i-1);//the smallest number with i digits
+            long long right=pow(10,i)-1;//the largest number with i digits
             
-            if(left>n){
+            if(left>n){//break since larger digit lengths aren't possible
                 break;
             }
-            long long numbers=min(right,n)-left+1;
-            ans+=(commas*numbers);
+            long long numbers=min(right,n)-left+1;//count how many numbers from this range lies within [1,n]
+            ans+=(commas*numbers);//add the contributions
         }
         return ans;
     }
